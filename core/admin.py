12e7a9f4ea9063d31b4498e96b1f51a84b71c2c4
vivek <vivekwarkade000@gmail.com>
2023-02-21 00:58:46 +0530

    fixture for dumpdata

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Course,Module,Subject
 # Register your models here.
+
+admin.site.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
+
+
+
