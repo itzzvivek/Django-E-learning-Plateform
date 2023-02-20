9b43bf946a901639ba82c34ef5873b5a4ff34b8b
vivek <vivekwarkade000@gmail.com>
2023-02-21 05:10:53 +0530

    **

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 # Create your models here.
 
 class Subject (models.Model):
@@ -32,8 +33,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name="modules",on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+    
+    class Meta:
+        ordering = ['order']
+        
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
     
 class Content(models.Model):
     module = models.ForeignKey(Module,related_name='contents',on_delete=models.CASCADE)
@@ -43,6 +49,10 @@ class Content(models.Model):
                                      )})
     object_id = models.PositiveBigIntegerField()
     item = GenericForeignKey('content_type','object_id')
+    order = OrderField(blank=True, for_fields=['moduls'])
+
+    class Meta:
+        ordering = ['order']
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User,related_name='%(class)s_related',on_delete=models.CASCADE)
