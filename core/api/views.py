b9e149beaf5335da2acc486970b30b5baf539ed0
vivek <vivekwarkade000@gmail.com>
2023-03-15 15:22:43 +0530

    viewset router

--- core/api/views.py
+++ core/api/views.py
@@ -5,9 +5,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import SubjectSerializer
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated 
+from rest_framework import viewsets
+from .serializers import CourseSerializer
 
 class CourseEnrollView(APIView):
     authentication_classes = (BasicAuthentication)
+    permission_classes = (IsAuthenticated)
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -22,3 +26,7 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.studets.add(request.user)
         return Response({'enrolled':True})
+    
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
