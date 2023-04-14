64ad57757a8d0d845a76003672ea12cfc27bfc67
vivek <vivekwarkade000@gmail.com>
2023-04-14 17:06:03 +0530

    register/login template

--- Django_Elearning/urls.py
+++ Django_Elearning/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path,include
+from . import views,user_login
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,6 +23,8 @@ urlpatterns = [
     path('base',views.BASE,name='base'),
     path('single_course',views.Single_Course,name='single-course'),
     path('about',views.about,name='about'),
-    path('contact',views.contact,name='contact')
+    path('contact',views.contact,name='contact'),
+    path('account/register',user_login.register,name='register'),
+    path('account/',include('django.contrib.auth.urls'))
     
 ]
--- Django_Elearning/views.py
+++ Django_Elearning/views.py
@@ -14,3 +14,4 @@ def about(request):
 
 def contact(request):
     return render(request,'main/contact.html')
+
