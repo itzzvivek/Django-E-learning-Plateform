1a2ab59fadefa9cbce6c689f7d868ce057a53984
vivek <vivekwarkade000@gmail.com>
2023-04-19 22:32:33 +0530

    create categories

--- Django_Elearning/views.py
+++ Django_Elearning/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import redirect,render
+from core.models import Categories
 
 def BASE(request):
     return render(request,'base.html')
 
 def HOME(request):
-    return render(request,'main/home.html')
+    category = Categories.objects.all().order_by('id')[0:5]
+    context={
+        'category':category
+    }
+    return render(request,'main/home.html', context)
 
 def Single_Course(request):
     return render(request,'main/single_course.html')
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Categories
 
 # Register your models here.
+
+admin.site.register(Categories)
