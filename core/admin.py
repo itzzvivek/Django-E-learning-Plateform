32de57a26e70cd2ea84953f9c262db99ed1e2398
vivek <vivekwarkade000@gmail.com>
2023-05-07 17:20:20 +0530

    course details

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Categories,Author,Course,Level
+from .models import Categories,Author,Course,Level,What_you_learn,Requirements
 
 # Register your models here.
 
+class What_you_learn_TabularInline(admin.TabularInline):
+    model = What_you_learn
+
+class Requirements_TabularInline(admin.TabularInline):
+    model = Requirements
+
+class course_admin(admin.ModelAdmin):
+    inlines = (What_you_learn_TabularInline,Requirements_TabularInline)
+
 admin.site.register(Categories)
 admin.site.register(Author)
-admin.site.register(Course)
+admin.site.register(Course,course_admin)
 admin.site.register(Level)
+admin.site.register(What_you_learn)
+admin.site.register(Requirements)
--- core/models.py
+++ core/models.py
@@ -69,3 +69,17 @@ def pre_save_post_receiver(sender, instance, *args,**kwargs):
     if not instance.slug:
         instance.slug = create_slug(instance)
 pre_save.connect(pre_save_post_receiver,Course)
+
+class What_you_learn(models.Model):
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    points = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.points
+    
+class Requirements(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    points = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.points
